0ea1db15d461764a3badd7817468c430c639418a
Whitney Grenaway <wmgrenaway@search1media.com>
2023-05-28 08:29:56 -0400

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,12 +4,19 @@ const About = () => {
     return (
         <section className="about">
             <h2>About Me</h2>
+
             <p>
                 As a self-motivated individual on the path to becoming a blockchain developer, I am driven by a strong determination to succeed in this field. Through self-teaching, I have wholeheartedly dedicated myself to acquiring the essential skills and technologies needed for proficient blockchain development. My unwavering commitment and tenacity have empowered me to overcome various challenges that have come my way, shaping me into a resilient learner.
             </p>
+
+            <br />
+
             <p>
                 During my journey, I have not only completed several tutorials and personal projects but have also sought to push my boundaries further. While these experiences have been valuable in building a solid foundation, I am now eagerly seeking opportunities to gain practical experience and contribute to real-world blockchain applications. It is through these hands-on encounters that I aim to further expand my knowledge and make meaningful contributions to the advancement of blockchain technology.
             </p>
+
+            <br />
+
             <p>
                 With a diligent work ethic, meticulous attention to detail, and an unyielding eagerness to learn and grow, I am confident in my potential to excel in any blockchain-related role. I am poised to leverage my skills and passion to contribute meaningfully to the dynamic and ever-evolving landscape of blockchain development.
             </p>
@@ -18,6 +25,11 @@ const About = () => {
 }
 
 export default About;
+
+
+
+
+
 
   
 
